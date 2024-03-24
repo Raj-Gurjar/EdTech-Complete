babf4a791994f080dc3d79eb309d0b9a2c436098
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2024-03-24 18:26:52 +0530

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,22 @@ import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 
 import Home from "./pages/Home/Home";
-import Navbar from './components/Navbar/Navbar'
+import Navbar from "./components/Navbar/Navbar";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
 
-function App() { 
+function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/contact' element = {<Contact/>} />
+        <Route path='/login' element = {<Login/>} />
+        <Route path='/signup' element = {<Signup/>} />
       </Routes>
     </div>
   );
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,8 +2,15 @@ import React from "react";
 import { NavbarLinks } from "../../data/Navbar-links";
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.scss";
+import { useSelector } from "react-redux";
+import { BsCart3 } from "react-icons/bs";
+import ProfileDropDown from "./ProfileDropDown";
 
 export default function Navbar() {
+  const { token } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.profile);
+  const { totalItems } = useSelector((state) => state.cart);
+
   return (
     <div className="bg-red-400 flex h-14 items-center justify-center border-b-[1px] border-b-black-700">
       <div className="navbar flex w-11/12 max-w-maxContent items-center justify-between">
@@ -25,9 +32,27 @@ export default function Navbar() {
           </ul>
         </nav>
 
+        {/* //! Log In /SignUp / DashBoard */}
         <div className="dynamic-buttons ">
-          <NavLink to="/login">Login</NavLink>
-          <NavLink to="/login">Login</NavLink>
+          {user && user?.accountType !== "Instructor" && (
+            <Link to="/dashboard/cart" className="relative">
+              <BsCart3 />
+              {totalItems > 0 && <span>{totalItems}</span>}
+            </Link>
+          )}
+
+          {token === null && (
+            <div className="auth-buttons flex gap-x-5">
+              <NavLink to="/login">
+                <button>Login</button>
+              </NavLink>
+              <NavLink to="/signup">
+                <button>Sign Up</button>
+              </NavLink>
+            </div>
+          )}
+
+          {token !== null && <ProfileDropDown />}
         </div>
       </div>
     </div>
