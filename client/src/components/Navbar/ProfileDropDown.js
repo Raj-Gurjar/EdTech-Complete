6c21a660c46b6be594c85fc88a35d9286ea88631
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2024-03-25 13:39:50 +0530

    added profile dropdown

--- client/src/components/Navbar/ProfileDropDown.js
+++ client/src/components/Navbar/ProfileDropDown.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 import { RxAvatar } from "react-icons/rx";
-
+import { Link } from "react-router-dom";
 
 export default function ProfileDropDown() {
+  const [profile, toggleProfile] = useState(false);
+
   return (
     <div>
-        <div className="profile-pic">
-            <RxAvatar className="text-xl cursor-pointer hover:bg-green-200"/>
-        </div>
-        <div className="profile-dropDown">
-            
-        </div>
+      <div className="profile-pic">
+        <RxAvatar
+          className="text-xl cursor-pointer hover:bg-green-200"
+          onClick={() => toggleProfile(!profile)}
+        />
+      </div>
+      {profile && <ul className="profile-dropDown bg-slate-500 absolute top-[50px] right-5">
+        <li><Link to="/dashboard">DashBoard</Link></li>
+        <li><Link to="/dashboard">Edit Profile</Link></li>
+        <li><Link to="/dashboard">Setting</Link></li>
+        <li><Link to="/dashboard">LogOut</Link></li>
+        </ul>}
     </div>
-  )
+  );
 }
